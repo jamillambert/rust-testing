41c7ab35051ad55e28ef23ffc617dea559f6a65d
Jamil Lambert <jamil.lambert@proton.me>
2024-07-11 15:43:18 +0100

    Add closure with clone type parameter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let foo = |s: &mut String| -> String {s.push('.'); s.clone()};
+    let mut bar = String::from("Hmm");
+    bar = foo(&mut bar.clone());
+    bar = foo(&mut bar.clone());
+    bar.push('.');
+    println!("bar = {}", bar);
 }
