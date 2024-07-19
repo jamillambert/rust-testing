3ab7d55518cebca4eabc1a4cc9358479c4c163bd
Jamil Lambert <jamil.lambert@proton.me>
2024-07-19 21:34:43 +0100

    Macro testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,121 @@
+macro_rules! define_extension_trait {
+    // With self, no generics.
+    ($(#[$($trait_attrs:tt)*])* $trait_vis:vis trait $trait_name:ident impl for $ty:ident {
+        $(
+            $(#[$($fn_attrs:tt)*])*
+            fn $fn:ident($slf:ident $(, $param_name:ident: $param_type:ty)*) $( -> $ret:ty )? $body:block
+        )*
+    }) => {
+        $(#[$($trait_attrs)*])* $trait_vis trait $trait_name {
+            $(
+                $(#[$($fn_attrs)*])*
+                fn $fn($slf $(, $param_name: $param_type)*) $( -> $ret )?;
+            )*
+        }
+
+        impl $trait_name for $ty {
+            $(
+                fn $fn($slf $(, $param_name: $param_type)*) $( -> $ret )? $body
+            )*
+        }
+    };
+    // With &self, no generics.
+    ($(#[$($trait_attrs:tt)*])* $trait_vis:vis trait $trait_name:ident impl for $ty:ident {
+        $(
+            $(#[$($fn_attrs:tt)*])*
+            fn $fn:ident(&$slf:ident $(, $param_name:ident: $param_type:ty)*) $( -> $ret:ty )? $body:block
+        )*
+    }) => {
+        $(#[$($trait_attrs)*])* $trait_vis trait $trait_name {
+            $(
+                $(#[$($fn_attrs)*])*
+                fn $fn(&$slf $(, $param_name: $param_type)*) $( -> $ret )?;
+            )*
+        }
+
+        impl $trait_name for $ty {
+            $(
+                fn $fn(&$slf $(, $param_name: $param_type)*) $( -> $ret )? $body
+            )*
+        }
+    };
+    // With &self, with generics.
+    ($(#[$($trait_attrs:tt)*])* $trait_vis:vis trait $trait_name:ident impl for $ty:ident {
+        $(
+            $(#[$($fn_attrs:tt)*])*
+            fn $fn:ident$(<$($gen:ident: $gent:ident),*>)?(&$slf:ident $(, $param_name:ident: $param_type:ty)*) $( -> $ret:ty )? $body:block
+        )*
+    }) => {
+        $(#[$($trait_attrs)*])* $trait_vis trait $trait_name {
+            $(
+                $(#[$($fn_attrs)*])*
+                fn $fn$(<$($gen: $gent),*>)?(&$slf $(, $param_name: $param_type)*) $( -> $ret )?;
+            )*
+        }
+
+        impl $trait_name for $ty {
+            $(
+                fn $fn$(<$($gen: $gent),*>)?(&$slf $(, $param_name: $param_type)*) $( -> $ret )? $body
+            )*
+        }
+    };
+    // No self, with generics.
+    ($(#[$($trait_attrs:tt)*])* $trait_vis:vis trait $trait_name:ident impl for $ty:ident {
+        $(
+            $(#[$($fn_attrs:tt)*])*
+            fn $fn:ident$(<$($gen:ident: $gent:ident),*>)?($($param_name:ident: $param_type:ty),*) $( -> $ret:ty )? $body:block
+        )*
+    }) => {
+        $(#[$($trait_attrs)*])* $trait_vis trait $trait_name {
+            $(
+                $(#[$($fn_attrs)*])*
+                fn $fn$(<$($gen: $gent),*>)?($($param_name: $param_type),*) $( -> $ret )?;
+            )*
+        }
+
+        impl $trait_name for $ty {
+            $(
+                fn $fn$(<$($gen: $gent),*>)?($($param_name: $param_type),*) $( -> $ret )? $body
+            )*
+        }
+    };
+    // No self, with generic `<T: AsRef<PushBytes>>`
+    ($(#[$($trait_attrs:tt)*])* $trait_vis:vis trait $trait_name:ident impl for $ty:ident {
+        $(
+            $(#[$($fn_attrs:tt)*])*
+            fn $fn:ident<T: AsRef<PushBytes>>($($param_name:ident: $param_type:ty),*) $( -> $ret:ty )? $body:block
+        )*
+    }) => {
+        $(#[$($trait_attrs)*])* $trait_vis trait $trait_name {
+            $(
+                $(#[$($fn_attrs)*])*
+                fn $fn<T: AsRef<PushBytes>>($($param_name: $param_type),*) $( -> $ret )?;
+            )*
+        }
+
+        impl $trait_name for $ty {
+            $(
+                fn $fn<T: AsRef<PushBytes>>($($param_name: $param_type),*) $( -> $ret )? $body
+            )*
+        }
+    };
+}
+
+
+define_extension_trait! {
+    pub trait ScriptBufExt impl for String {
+        fn new_p2pk(pubkey: u64) -> Self {
+            format!("OP_PUSHBYTES_{}", pubkey)
+        }
+
+        fn new_p2pkh(pubkey_hash: String) -> Self {
+            format!("Hash_{}", pubkey_hash)
+        }
+    }
+}
+
+
+
 fn main() {
     let foo = |s: &mut String| -> String {
         s.push('.');
@@ -9,3 +127,4 @@ fn main() {
     bar.push('.');
     println!("bar = {}", bar);
 }
+
