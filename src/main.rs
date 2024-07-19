83f41cbeb3f08bdd411c7f9323923475fb09fc7a
Jamil Lambert <jamil.lambert@proton.me>
2024-07-19 21:36:21 +0100

    foo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+
+/// A macro to define extension traits for a given type.
+///
+/// This macro allows defining extension traits for a type with various combinations of `self` and generics.
+/// It generates the trait definition and the corresponding implementation for the given type.
+///
+/// # Examples
+///
+/// ```
+/// define_extension_trait! {
+///     pub trait ScriptBufExt impl for String {
+///         fn new_p2pk(pubkey: u64) -> Self {
+///             format!("OP_PUSHBYTES_{}", pubkey)
+///         }
+///
+///         fn new_p2pkh(pubkey_hash: String) -> Self {
+///             format!("Hash_{}", pubkey_hash)
+///         }
+///     }
+/// }
+/// ```
 macro_rules! define_extension_trait {
     // With self, no generics.
     ($(#[$($trait_attrs:tt)*])* $trait_vis:vis trait $trait_name:ident impl for $ty:ident {
@@ -101,20 +122,36 @@ macro_rules! define_extension_trait {
     };
 }
 
-
 define_extension_trait! {
+    /// An extension trait for the `String` type that provides additional methods for working with script buffers.
     pub trait ScriptBufExt impl for String {
+        /// Creates a new script buffer for a pay-to-public-key (P2PK) script.
+        ///
+        /// # Arguments
+        ///
+        /// * `pubkey` - The public key to be used in the script.
+        ///
+        /// # Returns
+        ///
+        /// The generated script buffer.
         fn new_p2pk(pubkey: u64) -> Self {
             format!("OP_PUSHBYTES_{}", pubkey)
         }
 
+        /// Creates a new script buffer for a pay-to-public-key-hash (P2PKH) script.
+        ///
+        /// # Arguments
+        ///
+        /// * `pubkey_hash` - The public key hash to be used in the script.
+        ///
+        /// # Returns
+        ///
+        /// The generated script buffer.
         fn new_p2pkh(pubkey_hash: String) -> Self {
             format!("Hash_{}", pubkey_hash)
         }
     }
 }
-
-
 
 fn main() {
     let foo = |s: &mut String| -> String {
@@ -127,4 +164,3 @@ fn main() {
     bar.push('.');
     println!("bar = {}", bar);
 }
-
