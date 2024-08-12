796999423b27717f892725a40940dea74f850aaa
Jamil Lambert <jamil.lambert@proton.me>
2024-08-12 21:39:20 +0100

    Add verify function
    Add a function to verify transactions.  Current version is generated by
    copilot and needs to be fixed.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use bitcoin::absolute::LockTime;
 use bitcoin::blockdata::script::Script;
-use bitcoin::blockdata::transaction::{OutPoint, Transaction, TxIn, TxOut, Version};
+use bitcoin::blockdata::transaction::{OutPoint, Transaction, TxIn, TxOut, Version, Sequence};
 use bitcoin::secp256k1::{rand, Secp256k1};
 use bitcoin::{Address, Amount, Network, PublicKey, Txid, Witness};
 use bitcoin_hashes::sha256d::Hash;
@@ -32,7 +32,7 @@ fn create_bitcoin_transaction() {
         txid: Txid::from_raw_hash(*prev_txid),
         vout: 0,
     };
-    use bitcoin::blockdata::transaction::Sequence;
+
 
     let input = TxIn {
         previous_output: prev_outpoint,
@@ -61,6 +61,108 @@ fn create_bitcoin_transaction() {
     println!("Bitcoin Transaction: {:?}", transaction);
 }
 
+fn verify_transaction(transaction: &Transaction, prev_tx: &Transaction) -> bool {
+    // Verify the transaction
+    let mut input_scripts = vec![];
+    for input in &transaction.input {
+        let prev_txid = input.previous_output.txid;
+        let prev_vout = input.previous_output.vout;
+        let prev_output = prev_tx.output[prev_vout as usize].clone();
+        let script_pubkey = prev_output.script_pubkey.clone();
+        input_scripts.push(script_pubkey);
+    }
+
+    for (input, script_pubkey) in transaction.input.iter().zip(input_scripts) {
+        let script_sig = input.script_sig.clone();
+        let witness = input.witness.clone();
+        let mut script = script_sig.to_bytes();
+        //script.extend(witness.iter().flat_map(|w| w.to_bytes()));
+        let mut stack = vec![];
+        let mut altstack = vec![];
+        let mut pc = 0;
+        while pc < script.len() {
+            let opcode = script[pc];
+            pc += 1;
+            match opcode {
+                opcodes::all::OP_PUSHBYTES_0..=opcodes::all::OP_PUSHBYTES_75 => {
+                    let len = opcode as usize;
+                    stack.push(script[pc..pc + len].to_vec());
+                    pc += len;
+                }
+                opcodes::all::OP_PUSHDATA1 => {
+                    let len = script[pc] as usize;
+                    pc += 1;
+                    stack.push(script[pc..pc + len].to_vec());
+                    pc += len;
+                }
+                opcodes::all::OP_PUSHDATA2 => {
+                    let len = u16::from_le_bytes([script[pc], script[pc + 1]]) as usize;
+                    pc += 2;
+                    stack.push(script[pc..pc + len].to_vec());
+                    pc += len;
+                }
+                opcodes::all::OP_PUSHDATA4 => {
+                    let len = u32::from_le_bytes([
+                        script[pc],
+                        script[pc + 1],
+                        script[pc + 2],
+                        script[pc + 3],
+                    ]) as usize;
+                    pc += 4;
+                    stack.push(script[pc..pc + len].to_vec());
+                    pc += len;
+                }
+                opcodes::all::OP_DUP => {
+                    if let Some(item) = stack.last().cloned() {
+                        stack.push(item);
+                    }
+                }
+                opcodes::all::OP_HASH160 => {
+                    if let Some(item) = stack.pop() {
+                        let hash = bitcoin_hashes::Hash::hash(&item);
+                        stack.push(hash.into_inner().to_vec());
+                    }
+                }
+                opcodes::all::OP_EQUALVERIFY => {
+                    if let (Some(item1), Some(item2)) = (stack.pop(), stack.pop()) {
+                        if item1 == item2 {
+                            continue;
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                opcodes::all::OP_CHECKSIG => {
+                    if let (Some(pubkey), Some(sig)) = (stack.pop(), stack.pop()) {
+                        let pubkey = bitcoin::PublicKey::from_slice(&pubkey);
+                        let sig = bitcoin::Signature::from_der(&sig);
+                        if let (Ok(pubkey), Ok(sig)) = (pubkey, sig) {
+                            let tx = transaction.clone();
+                            let sighash = tx.signature_hash(0, &script_pubkey, bitcoin::SigHashType::All).unwrap();
+                            let secp = bitcoin::secp256k1::Secp256k1::new();
+                            if secp.verify(&sighash, &sig, &pubkey.key).is_ok() {
+                                stack.push(vec![0x01]);
+                            } else {
+                                stack.push(vec![0x00]);
+                            }
+                        } else {
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+                _ => {
+                    return false;
+                }
+            }
+        }
+    }
+
+    true
+}
 fn main() {
     // Generate random key pair.
     let s = Secp256k1::new();
